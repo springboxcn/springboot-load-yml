7f73fd25002220da09a2ff4b7d860c87af6de209
maskleo <masque@aliyun.com>
2018-06-16 22:18:26 +0800

    init

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ springboot load yml look YamlPropertySourceLoader
 
 
 https://my.oschina.net/bfleeee/blog/879209
+
+## LICENSE
+
+### [CC-BY-SA-3.0](https://creativecommons.org/licenses/by-nc-sa/3.0/cn/)
+
+[![](LICENSE.png)](https://creativecommons.org/licenses/by-nc-sa/3.0/cn/)
---
+++ src/main/java/edu/maskleo/springbootloadyml/SpringbootLoadYmlApplication.java
@@ -0,0 +1,12 @@
+package edu.maskleo.springbootloadyml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLoadYmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLoadYmlApplication.class, args);
+	}
+}
